5000b7bfc275243f1df149bf05c33f9752241b6d
vicky <vcmartin2020@gmail.com>
2021-08-29 15:30:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+    </form>
+    </div>
+  );
+}
+
+export default App;
